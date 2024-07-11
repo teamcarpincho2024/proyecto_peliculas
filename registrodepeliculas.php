3d6485170086bffbedc4662fe74bac319ee4c5c1
teamcarpincho2024 <cristian_corsa@hotmail.com>
2024-07-11 00:21:11 -0300

    Update registrodepeliculas.php

--- registrodepeliculas.php
+++ registrodepeliculas.php
@@ -1,3 +1,18 @@
+
+<?php
+$conexion = mysqli_connect("localhost","id22032743_dbpeliculas","W!89H*V3#e","id22032743_guille");
+if (mysqli_connect_errno()) {
+  echo '<p>ERROR: no se pudo conectar a la BD</p>';
+  exit;
+} else {
+  //echo '<p>Conexión correcta a la BD</p>';
+  $direcs = mysqli_query($conexion, 'select id_director, nombre, apellido from directores');
+}
+
+if (isset($_GET['accion']) && !empty($_GET['accion'])) {$accion = $_GET['accion'];} else {$accion = "insertar";}
+if (isset($_GET['id_movie']) && !empty($_GET['id_movie']) && is_numeric($_GET['id_movie'])) {$id_movie = (int)$_GET['id_movie'];} else {$id_movie = "";}
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -30,64 +45,52 @@
         <section class="seccionregistrodepeliculas">
             
             <h2 class="tituloform">Registro de Películas</h2>
-<!---https://lavagnino.000webhostapp.com/registrodepeliculas2.php--->
-            <form action="registrodepeliculas2.php" method="post" id="formregistrodepeliculas">
+
+            <form action="https://lavagnino.000webhostapp.com/registrodepeliculas2.php" method="post" id="formregistrodepeliculas" class="padding:20px">
+                <input type="hidden" name="accion" value="<?php echo $accion; ?>">
+                <input type="hidden" name="id_movie" value="<?php echo $id_movie; ?>">
                 <div class="camposoblig"><label>Todos los campos son obligatorios</label></div>
 
-                <div>
-                <?php
-$conexion = mysqli_connect("localhost","id22032743_dbpeliculas","W!89H*V3#e","id22032743_guille");
-if (mysqli_connect_errno()) {
-  echo '<p>ERROR: no se pudo conectar a la BD</p>';
-} else {
-  //echo '<p>Conexión correcta a la BD</p>';
-  $direcs = mysqli_query($conexion, 'select id_director, nombre, apellido from directores');
-}
-                ?>
-                    <select name="id_director" id="id_director">
+                <div class="box">
+                    <select class="form-select form-select-sm" style="margin:20px 20px 20px 15px; width:80%;" aria-label="Default select example" style="margin:20px;" aria-label="Default select example" name="id_director" id="id_director">
                         <option value="">Ingresa un director</option>
                     <?php
                       while ($dire = mysqli_fetch_array($direcs)) {
-                        echo '<option value="'.$dire['id_director'].'">'.$dire['nombre'].' '.$dire['apellido'].'</option>';
+                        echo '<option ';
+                        if (isset($_GET['id_director']) && !empty($_GET['id_director']) && $_GET['id_director'] == $dire['id_director']) {echo 'selected="selected" ';}
+                        echo 'value="'.$dire['id_director'].'">'.$dire['nombre'].' '.$dire['apellido'].'</option>';
                       }
                     ?>
                     </select>
                     <div class="error-text"></div>
                 </div>
 
-                <div>
-                    <input class="campo cform" type="text" maxlength="40" size="40" placeholder="Ingresa el Nombre" id="nombre" name="nombre">
-                    <div class="error-text"></div>
+                <div class="contcamporeg">
+                    <input class="campo cform" type="text" maxlength="40" size="40" placeholder="Ingresa el Nombre" id="nombre" name="nombre" value="<?php if (isset($_GET['nombre']) && !empty($_GET['nombre'])) {echo $_GET['nombre'];} ?>">
                 </div>
-                <div>
-                    <input class="campo cform" type="text" maxlength="40" size="40" placeholder="Ingresa una Descripcion" id="descripcion" name="descripcion">
-                    <div class="error-text"></div>
+                <div class="contcamporeg">
+                    <input class="campo cform" type="text" maxlength="40" size="40" placeholder="Ingresa una Descripcion" id="descripcion" name="descripcion" value="<?php if (isset($_GET['descripcion']) && !empty($_GET['descripcion'])) {echo $_GET['descripcion'];} ?>">
                 </div>
-                <div>
-                    <input class="campo cform" type="text" maxlength="40" size="40" placeholder="Ingresa el Genero" id="genero" name=" genero">
-                    <div class="error-text"></div>
+                <div class="contcamporeg">
+                    <input class="campo cform" type="text" maxlength="40" size="40" placeholder="Ingresa el Genero" id="genero" name="genero" value="<?php if (isset($_GET['genero']) && !empty($_GET['genero'])) {echo $_GET['genero'];} ?>">
                 </div>
-                <div>
-                    <input class="campo cform" type="text" maxlength="40" size="40" placeholder="Ingresa Calificacion" id="calificacion" name="calificacion">
-                    <div class="error-text"></div>
+                <div class="contcamporeg">
+                    <input class="campo cform" type="text" maxlength="40" size="40" placeholder="Ingresa Calificacion" id="calificacion" name="calificacion" value="<?php if (isset($_GET['calificacion']) && !empty($_GET['calificacion'])) {echo $_GET['calificacion'];} ?>">
                 </div>
-                <div>
-                    <input class="campo cform" type="number" maxlength="4" size="40" placeholder="Ingresa el Anio" id="anio" name="anio">
-                    <div class="error-text"></div>
+                <div class="contcamporeg">
+                    <input class="campo cform" type="number" maxlength="4" size="40" placeholder="Ingresa el Anio" id="anio" name="anio" value="<?php if (isset($_GET['anio']) && !empty($_GET['anio'])) {echo $_GET['anio'];} ?>">
                 </div>
-                <div>
-                    <input class="campo cform" type="number" maxlength="1" size="40" placeholder="Ingresa Estrellas" id="estrellas" name="estrellas">
-                    <div class="error-text"></div>
+                <div class="contcamporeg">
+                    <input class="campo cform" type="number" maxlength="1" size="40" placeholder="Ingresa Estrellas" id="estrellas" name="estrellas" value="<?php if (isset($_GET['estrellas']) && !empty($_GET['estrellas'])) {echo $_GET['estrellas'];} ?>">
                 </div>
-                <div>
-                    <input class="campo cform" type="password" maxlength="40" size="40" placeholder="Email de usuario" id="correo" name="correo">
-                    <div class="error-text"></div>
+                <div class="contcamporeg">
+                    <input class="campo cform" type="password" maxlength="40" size="40" placeholder="Contraseña de usuario" id="correo" name="correo">
                 </div>
-                <div id="terminos">
+                <div class="contcamporeg" id="terminos">
                   <input id="terminos" type="checkbox" checked="checked" />
                   <label for="terminos" class="form_terminos_texto">Acepto términos y condiciones</label>
                 </div>
-                <div>
+                <div class="contcamporeg">
                     <button class="naveg hvr-grow">Enviar datos</button>
                 </div>
             </form>
